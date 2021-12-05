3a454dccdd59ac8d78625163539c2e6f12784273
bel-lov <84201835+bel-lov@users.noreply.github.com>
2021-12-05 09:23:16 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var popup=document.getElementById('popup');
+var popupClose=document.getElementById('popup-close');
+popupClose.addEventListener("click",toggle popup,false');
+function togglePopup(){
+  popup.classList.toggle('shown');
+}
